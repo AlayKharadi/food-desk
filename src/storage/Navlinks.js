a7cd0f95f36710cf0fde8f56dd398492b17b9138
AlayKharadi <62132315+AlayKharadi@users.noreply.github.com>
2021-11-06 19:47:19 +0530

    LogIn system

---
+++ src/storage/Navlinks.js
@@ -0,0 +1,7 @@
+import Menu from '../pages/Menu';
+
+export const navlinks = [
+    { id: 0, path: '/Home', name: 'Home', target: <Menu /> },
+    { id: 1, path: '/Offers', name: 'Offers', target: <Menu /> },
+    { id: 2, path: '/Settings', name: 'Settings', target: <Menu /> }
+];
