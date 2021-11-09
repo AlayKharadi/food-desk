4af2a1a1d7b9b2e562a13c2dd06d94392545743c
AlayKharadi <62132315+AlayKharadi@users.noreply.github.com>
2021-11-09 14:27:58 +0530

    encryption layer and code restructure

--- src/storage/Navlinks.js
+++
@@ -1,7 +0,0 @@
-import Menu from '../pages/Menu';
-
-export const navlinks = [
-    { id: 0, path: '/Home', name: 'Home', target: <Menu /> },
-    { id: 1, path: '/Offers', name: 'Offers', target: <Menu /> },
-    { id: 2, path: '/Settings', name: 'Settings', target: <Menu /> }
-];
