53bce434bbc51569bb175ad187ee8afcaef3e5fb
Alay Kharadi <alay.kharadi3@gmail.com>
2021-11-11 22:18:57 +0530

    signup and forgot password

--- routes/sendotp.js
+++ routes/sendotp.js
@@ -5,41 +5,43 @@ const sendotp = Router();
 const transporter = require('../config/mail');
 const RandExp = require('randexp');
 
-sendotp.post('/sendotp', (req, res) => {
-	pool.query(`SELECT email FROM users WHERE username = '${req.body.user}';`, (error, results) => {
-		if (error) {
-			res.status(500).send(error);
+function sendmail(email, res) {
+	const otp = new RandExp(/^[0-9a-zA-Z]{10}$/).gen();
+	const mailOptions = {
+		from: `${process.env.EMAIL_ADDRESS}`,
+		to: `${email}`,
+		subject: 'Link To Reset Password',
+		text:
+			'This email is for the password change for the associated account with this email. you (or someone else) have requested the reset of the password.\n\n'
+			+ 'Please note down blow otp and write it in otp section.\n\n'
+			+ `OTP : ${otp}\n\n`
+			+ 'If you did not request this, please ignore this email.\n',
+	};
+	transporter.sendMail(mailOptions, (err, _response) => {
+		if (err) {
+			res.status(500).send(err);
 		}
-		if (results.rows.length > 0) {
-
-			const otp = new RandExp(/^[0-9a-zA-Z]{10}$/).gen();
-
-			const mailOptions = {
-				from: `${process.env.EMAIL_ADDRESS}`,
-				to: `${results.rows[0].email}`,
-				subject: 'Link To Reset Password',
-				text:
-					'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
-					+ 'Please note down blow otp and write it in otp section.\n\n'
-					+ `OTP : ${otp}`
-					+ 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
-			};
-
-			console.log('sending mail');
-
-			transporter.sendMail(mailOptions, (err, _response) => {
-				if (err) {
-					res.status(500).send(err);
-				} else {
-					res.status(200).send({ otp });
-				}
-			});
-
-		} else {
-			res.status(500).send(undefined);
-		}
+		res.status(200).send({ otp });
 	});
-})
+}
 
+sendotp.post('/sendotp', (req, res) => {
+	if (req.body.type === "user") {
+		pool.query(`SELECT email FROM users WHERE username = '${req.body.value}';`, (error, results) => {
+			if (error) {
+				res.status(500).send(error);
+			}
+			if (results.rows.length > 0) {
+				console.log(results.rows);
+				sendmail(results.rows[0].email, res);
+			} else {
+				res.status(500).send(undefined);
+			}
+		});
+	}
+	if (req.body.type === "email") {
+		sendmail(req.body.value, res);
+	}
+})
 
 module.exports = sendotp;
