ba3642f4888b90f5a9f76b447d5007d949e4fbe7
Alay Kharadi <alay.kharadi3@gmail.com>
2021-12-05 18:53:00 +0530

    Menu Page backend

--- client/src/storage/Navlinks.js
+++ client/src/storage/Navlinks.js
@@ -3,9 +3,9 @@ import Cart from '../pages/Cart';
 import Checkout from '../pages/Checkout';
 
 export const navlinks = [
-    { id: 0, path: '/Home', name: 'Home', target: <Menu /> },
+    { id: 0, path: '/Menu', name: 'Menu', target: <Menu /> },
     { id: 1, path: '/Offers', name: 'Offers', target: <Menu /> },
-    { id: 2, path: '/Settings', name: 'Settings', target: <Menu /> },
-    { id: 3, path: '/Cart', name: 'Cart', target: <Cart /> },
-    { id: 4, path: '/Checkout', name: 'Checkout', target: <Checkout /> }
+    { id: 2, path: '/Cart', name: 'Cart', target: <Cart /> },
+    { id: 3, path: '/Checkout', name: 'Checkout', target: <Checkout /> },
+    { id: 4, path: '/Settings', name: 'Settings', target: <Menu /> }
 ];
