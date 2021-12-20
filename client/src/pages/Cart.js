36c1b323ec733adfba709b4625e126c9e9c66695
AlayKharadi <62132315+AlayKharadi@users.noreply.github.com>
2021-12-20 13:52:29 +0530

    Payment Section Modification

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,11 +5,13 @@ import { Alert, AlertTitle, Container, Grid, IconButton } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import LocalMallTwoToneIcon from '@mui/icons-material/LocalMallTwoTone';
 import { userStore } from '../storage/store';
-import { REMOVE_FROM_CART } from '../storage/actiontype';
+import { ADD_PROMO, REMOVE_FROM_CART, REMOVE_PROMO } from '../storage/actiontype';
 import empty_cart from '../Images/empty_cart.svg';
 
 const Cart = () => {
     let cart = useSelector(state => state.cart);
+
+    let promo = useSelector(state => state.promo);
 
     const [coupon, setCoupon] = useState("");
 
@@ -18,6 +20,54 @@ const Cart = () => {
         const pattern = /^[0-9a-zA-Z]*$/;
         if (pattern.test(e.target.value)) {
             setCoupon(e.target.value);
+        }
+    }
+
+    function AddPromo(e) {
+        e.preventDefault();
+        if ((promo.length >= 0) && (promo.length < 3)) {
+            for (let i = 0; i < promo.length; i++) {
+                if (coupon === promo[i].name) {
+                    setCoupon("");
+                    window.alert("Promo Code Already used here.")
+                    return;
+                }
+            }
+            fetch('/API/promo', {
+                method: 'POST',
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    promo: coupon
+                })
+            }).then(response => {
+                //check the response
+                if (response.status === 500) {
+                    return undefined;
+                }
+                return response.json();
+            }).then(data => {
+                if (data === undefined) {
+                    window.alert("Promo Code not Found.");
+                    setCoupon("");
+                } else {
+                    window.alert("Promo Code successfully subscribed.");
+                    setCoupon("");
+                    userStore.dispatch({
+                        type: ADD_PROMO,
+                        payload: {
+                            name: data.name,
+                            value: data.price
+                        }
+                    });
+                    window.localStorage.setItem("promo", JSON.stringify(userStore.getState().promo));
+                }
+            })
+        } else {
+            window.alert("New PromoCode Not Allowed.");
+            setCoupon("");
         }
     }
 
@@ -35,17 +85,23 @@ const Cart = () => {
                         Cart is Empty
                     </Grid>
                     <Grid item xs={12}>
-                        <img src={empty_cart} placeholder="Empty cart" width="200vw"/>
+                        <img src={empty_cart} alt="Empty cart" width="200vw" />
                     </Grid>
                 </Grid>
             </Container>
         );
     }
 
-    let total = 0;
+    let total = 0, promo_total = 0;
 
-    for (let i = 0; i < cart.length; i++){
+    for (let i = 0; i < cart.length; i++) {
         total = total + (cart[i].price * cart[i].quantity);
+    }
+
+    if (promo !== undefined) {
+        for (let i = 0; i < promo.length; i++) {
+            promo_total = promo_total + promo[i].value;
+        }
     }
 
     return (
@@ -123,11 +179,61 @@ const Cart = () => {
                                         onChange={(e) => EditCoupon(e)}
                                     />
                                     <div className="input-group-append border-0">
-                                        <button id="button-addon3" type="button" className="btn btn-dark px-4 rounded-pill" title="Coupon">
+                                        <button
+                                            id="button-addon3"
+                                            type="button"
+                                            className="btn btn-primary px-4 rounded-pill"
+                                            title="Coupon"
+                                            onClick={(e) => AddPromo(e)}
+                                        >
                                             <LocalMallTwoToneIcon />
                                         </button>
                                     </div>
                                 </div>
+                                <Grid container spacing={2}>
+                                    {
+                                        (promo !== undefined)
+                                        &&
+                                        promo.map((item, index) => {
+                                            return (
+                                                <Grid
+                                                    item
+                                                    xs={12}
+                                                    key={index}
+                                                >
+                                                    <Alert
+                                                        severity="info"
+                                                        action={
+                                                            <IconButton
+                                                                aria-label="close"
+                                                                color="inherit"
+                                                                size="small"
+                                                                onClick={() => {
+                                                                    userStore.dispatch({
+                                                                        type: REMOVE_PROMO,
+                                                                        payload: {
+                                                                            name: item.name
+                                                                        }
+                                                                    });
+                                                                    window.localStorage.setItem("promo", JSON.stringify(userStore.getState().promo));
+                                                                }}
+                                                            >
+                                                                <CloseIcon fontSize="inherit" />
+                                                            </IconButton>
+                                                        }
+                                                    >
+                                                        <div className="text-success">
+                                                            <h6 className="my-0">Promo:
+                                                                <small>{item.name}</small>
+                                                            </h6>
+                                                        </div>
+                                                        <span className="text-success">{item.value}</span>
+                                                    </Alert>
+                                                </Grid>
+                                            );
+                                        })
+                                    }
+                                </Grid>
                             </div>
                         </div>
                         <div className="col-lg-6">
@@ -140,8 +246,8 @@ const Cart = () => {
                                         <strong>{total}</strong>
                                     </li>
                                     <li className="d-flex justify-content-between py-3 border-bottom">
-                                        <strong className="text-muted">Shipping and handling</strong>
-                                        <strong>10.00</strong>
+                                        <strong className="text-muted">Promo Code</strong>
+                                        <strong>{promo_total}</strong>
                                     </li>
                                     <li className="d-flex justify-content-between py-3 border-bottom">
                                         <strong className="text-muted">Tax</strong>
@@ -149,10 +255,20 @@ const Cart = () => {
                                     </li>
                                     <li className="d-flex justify-content-between py-3 border-bottom">
                                         <strong className="text-muted">Total</strong>
-                                        <h5 className="fw-bold">{total + 10}</h5>
+                                        <h5 className="fw-bold">{total - promo_total}</h5>
                                     </li>
                                 </ul>
-                                <Link className="btn btn-dark rounded-pill py-2 d-md-block" to='/Checkout'>Procceed to checkout</Link>
+                                <button className="btn btn-primary rounded-pill py-2 d-md-block" disabled={(total - promo_total) < 0}>
+                                    <Link
+                                        style={{
+                                            textDecoration: "none"
+                                        }}
+                                        className="text-light"
+                                        to='/Checkout'
+                                    >
+                                        Procceed to checkout
+                                    </Link>
+                                </button>
                             </div>
                         </div>
                     </div>
